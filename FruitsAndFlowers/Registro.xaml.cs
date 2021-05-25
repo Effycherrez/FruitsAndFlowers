ebfc38507e7908aed90681e6a1dc2090958c3e63
Andres Uquillas <andy2104@hotmail.es>
2021-05-25 14:29:02 -0500

    Registro configurado

--- FruitsAndFlowers/Registro.xaml.cs
+++ FruitsAndFlowers/Registro.xaml.cs
@@ -15,6 +15,26 @@ namespace FruitsAndFlowers
         public Registro()
         {
             InitializeComponent();
+            if (Application.Current.Properties["ProfilePicture"] != null)
+                imgPerfil.Source = Application.Current.Properties["ProfilePicture"].ToString();
+
+            if (Application.Current.Properties["Id"] != null)
+                lblId.Text = lblId.Text + Application.Current.Properties["Id"].ToString();
+
+            if (Application.Current.Properties["FirstName"] != null)
+                lblNombre.Text = lblNombre.Text + Application.Current.Properties["FirstName"].ToString();
+
+            if (Application.Current.Properties["LastName"] != null)
+                lblApellido.Text = lblApellido.Text + Application.Current.Properties["LastName"].ToString();
+
+            if (Application.Current.Properties["EmailAddress"] != null)
+                lblEmail.Text = lblEmail.Text + Application.Current.Properties["EmailAddress"].ToString();
+
+        }
+
+        private async void btnSalir_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
         }
     }
 }
